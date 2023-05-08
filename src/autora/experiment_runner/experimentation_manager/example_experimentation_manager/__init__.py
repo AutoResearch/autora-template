d617ef32d4688d67cc42b8f19a20f6affd6d1f42
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-05-08 20:08:46 +0000

    feat: add examples for experiment_runner, experimentation_manager and recruitement_manger

---
+++ src/autora/experiment_runner/experimentation_manager/example_experimentation_manager/__init__.py
@@ -0,0 +1,27 @@
+"""
+Example experimentaton manager:
+    This is an example of functions that could be included 
+    (but are not nessecarily included)
+"""
+def send_conditions(args, conditions):
+    """
+    Add a description here
+    Args:
+        args: the arguments needed to set up the experiment (for example credentials)
+        conditions: the conditions to be send
+    """
+    pass
+
+def check_status(args)
+    """
+    Add description here
+    """
+    pass
+
+def get_observations(args):
+    """
+    Add description here
+    """
+    observations = None
+    return observations
+    
---
+++ src/autora/experiment_runner/recruitment_manager/example_recruitment_manager/__init__.py
@@ -0,0 +1,29 @@
+"""
+Example recruitment manager:
+    This is an example of functions that could be included 
+    (but are not nessecarily included)
+"""
+def example_setup_experiment(args):
+    """
+    Add a description of the set up here
+    args:
+        the arguments needed to set up the experiment (for example credentials)
+    """
+    pass
+
+def start_experiment(args)
+    """
+    Add description here
+    """
+    pass
+
+def pause_experiment(args):
+    """
+    Add description here
+    """
+
+def check_status(args):
+    """
+    Add description here
+    """
+    pass
