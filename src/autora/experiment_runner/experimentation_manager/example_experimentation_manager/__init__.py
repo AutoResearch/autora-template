7c0c00a98db282aa0475c12370d8537c857568c0
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-05-09 05:44:14 -0400

    Update src/autora/experiment_runner/experimentation_manager/example_experimentation_manager/__init__.py
    Co-authored-by: benwandrew <benwallaceandrew@gmail.com>

--- src/autora/experiment_runner/experimentation_manager/example_experimentation_manager/__init__.py
+++ src/autora/experiment_runner/experimentation_manager/example_experimentation_manager/__init__.py
@@ -9,7 +9,7 @@ def send_conditions(args, conditions):
     """
     Add a description here
     Args:
-        args: the arguments needed to set up the experiment (for example credentials)
+        args: the arguments needed to set up the experiment (e.g., credentials)
         conditions: the conditions to be sent
     """
     pass
